e54494d22451328f3f7d106a851120ee29e8fe00
Marc Brookes <meermouse@hotmail.com>
2022-11-09 21:04:45 +0000

    added battlecanvas and viewer

---
+++ battleplankit/src/canvas/BattleCanvas.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+import ReactCanvasPaint from 'react-canvas-paint'
+import 'react-canvas-paint/dist/index.css'
+
+class BattleCanvas extends Component {
+  render() {
+    return <ReactCanvasPaint />
+  }
+}
---
+++ battleplankit/src/viewer/Viewer.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import BattleCanvas from '../canvas/BattleCanvas.jsx'
+
+class Viewer extends Component {
+  render() {
+    return <BattleCanvas />
+  }
+}
