edbaf0a8ca5297c57421f80b4bee63b73ccabd52
Marc Brookes <meermouse@hotmail.com>
2025-10-17 11:35:24 +0100

    chore: update package.json and add eslint configuration
    - Added new dependencies for ESLint and React plugins in package.json
    - Created a new eslint.config.js file with recommended settings and custom rules

---
+++ battleplankit/eslint.config.js
@@ -0,0 +1,34 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactPlugin from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": reactHooks,
+      "jsx-a11y": jsxA11y,
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off", // React 17+ doesn’t need React import
+      "react/prop-types": "off", // if you use TypeScript or prefer no PropTypes
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
